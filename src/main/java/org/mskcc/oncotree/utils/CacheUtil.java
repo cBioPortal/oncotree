dec356a781eacda371b05e9e1da41a1cbdbbb2af
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-05-23 11:34:30 -0400

    Add version support for tumorTypes
    Getting stable versions from github

--- src/main/java/org/mskcc/oncotree/utils/CacheUtil.java
+++ src/main/java/org/mskcc/oncotree/utils/CacheUtil.java
@@ -1,20 +1,38 @@
 package org.mskcc.oncotree.utils;
 
 import org.mskcc.oncotree.model.TumorType;
+import org.mskcc.oncotree.model.Version;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by Hongxin on 2/25/16.
  */
 public class CacheUtil {
-    public static Map<String, TumorType> tumorTypes = null;
+    public static Map<Version, Map<String, TumorType>> tumorTypes = new HashMap<>();
 
+    /**
+     * Get most up to date tumor types
+     *
+     * @return the tumor types
+     */
     public static Map<String, TumorType> getTumorTypes() {
-        //Don't cache tumor types file. It may be changed anytime.
-//        if (tumorTypes == null) {
-        tumorTypes = TumorTypesUtil.getTumorTypes();
-//        }
-        return tumorTypes;
+        return TumorTypesUtil.getTumorTypes();
+    }
+
+
+    public static Map<String, TumorType> getTumorTypesByVersion(String v) {
+        if (VersionUtil.hasVersion(v)) {
+            Version version = VersionUtil.getVersion(v);
+            if (tumorTypes.containsKey(v)) {
+                return tumorTypes.get(v);
+            } else {
+                tumorTypes.put(version, TumorTypesUtil.getTumorTypesByVersion(version));
+                return tumorTypes.get(version);
+            }
+        } else {
+            return null;
+        }
     }
 }
