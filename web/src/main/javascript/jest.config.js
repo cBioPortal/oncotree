afb4b99997e306a72ab05e552f95cb7f43daee27
bprize15 <148106517+bprize15@users.noreply.github.com>
2025-04-08 13:26:42 -0400

    use url from domain for backend requests (#294)
    * use url from domain for backend requests
    
    * fix tests (window is not defined)
    
    * use jsdom in test environment
    
    * remove unnecessary parentheses
    
    * add jsdom package

--- web/src/main/javascript/jest.config.js
+++ web/src/main/javascript/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-    testEnvironment: "node",
+    testEnvironment: "jsdom",
     transform: {
       "^.+.tsx?$": ["ts-jest", {}],
     },
